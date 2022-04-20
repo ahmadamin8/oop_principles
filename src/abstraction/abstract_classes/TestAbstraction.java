2da59af8fd9b09913ed1ca05a8ddff0f7ada7759
ahmadamin8 <ahmadaminmj23@gmail.com>
2022-04-19 20:56:37 -0500

    added abstract intro

---
+++ src/abstraction/abstract_classes/TestAbstraction.java
@@ -0,0 +1,48 @@
+package abstraction.abstract_classes;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TestAbstraction {
+    public static void main(String[] args) {
+
+        Samsung s1 = new Samsung("Samsung" , "Black" , 64, 700);
+        Nokia n1 = new Nokia("Nokia" , "Blue" , 4 , 100);
+        iPhone i1 = new iPhone("Apple" , "Silver" , 128 , 1000);
+
+        List<Phone> phones = new ArrayList<>();
+        phones.add(s1);
+        phones.add(n1);
+        phones.add(i1);
+
+        for (Phone element : phones) {
+            System.out.println(element);
+        }
+
+        int count = 0;
+        for (Phone element : phones) {
+            if(element.isConvertible()) count++;
+        }
+        System.out.println(count);
+
+        int max = Integer.MIN_VALUE;
+        Phone mostExpensive = null;
+
+
+        for (Phone phone : phones) {
+            if (phone.price > max) mostExpensive = phone;
+        }
+
+        System.out.println("The most expensive phone is = " + mostExpensive.getClass().getSimpleName());
+
+        System.out.println(iPhone.OS);
+        System.out.println(Samsung.OS);
+        System.out.println(Nokia.OS);
+
+
+
+
+
+
+    }
+}
